701302ef662f54d97f73c91a627a2f81b7296b90
tomerbuzaglo <tomer.bu@gmail.com>
2016-02-23 09:36:42 +0200

    removed the config folder from git

---
+++ README.md
@@ -0,0 +1,2 @@
+# PhotoUpload
+uploading images to heroku and amazon s3 bucket
--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 module.exports =
 {
-	'AWS_S3_KEY':'<Your Key>',
-	'AWS_S3_SECRET':'<Your Secret>',
-	'AWS_S3_BUCKET':'<Your Bucket on s3'
+	'AWS_S3_KEY':'AKIAI5GWLGIGEXYSUA6Q',
+	'AWS_S3_SECRET':'8lFInDmvkVEEeHx2YwhHibYJEYW7vmpf6maQtCrj',
+	'AWS_S3_BUCKET':'tomerbu.images'
 }
