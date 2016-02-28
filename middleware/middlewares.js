0c75268f57a3f26f4e8f8739f48c2483cdf6e57c
tomerbuzaglo <tomer.bu@gmail.com>
2016-02-28 11:55:24 +0200

    debugging

--- middleware/middlewares.js
+++ middleware/middlewares.js
@@ -5,15 +5,13 @@ var config = require('../configd/config.js');
 module.exports = {
   tokenVerifier: function(req, res, next) {
     // check header or url parameters or post parameters for token
-    var token = req.body.token ||req.query.token||req.params.token||req.headers.authorization|| req.headers['x-access-token'] || req.headers['Authorization'];
+    var token = req.body.token ||req.query.token||req.params.token||req.headers.authorization|| req.headers['Authorization']||req.headers['x-access-token'];
     if(!token)
-      console.error('Failed to aquire token');
-      console.error(req.headers);
     // decode token
     if (token) {
       jwt.verify(token, config.secret, function(err, decoded) {      
         if (err) {
-          res.send(401, 'missing authorization header');
+          res.send(401, 'token failed');
           return res.json({ success: false, message: 'Failed to authenticate token.' });    
         } else {
           // if everything is good, save to request for use in other routes
@@ -24,7 +22,7 @@ module.exports = {
     } else {
       // if there is no token
       // return an error
-      return res.status(403).send({ 
+      return res.status(401).send({ 
           success: false, 
           message: 'No token provided.' 
       });
