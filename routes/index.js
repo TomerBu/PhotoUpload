4caadb3d3bcaf6af8f154d0fbf97da08bcc7aeca
tomerbuzaglo <tomer.bu@gmail.com>
2016-02-29 10:29:48 +0200

    added bcrypt demo

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,35 @@
 //Unsecured route serving Welcome Page and a Playground.
 
 var express = require('express'),
-	router = express.Router(); 
-
+	router = express.Router(),
+	bcrypt = require('bcrypt');
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'ImagesApi' });
 });
 
+
+router.get('/homester/:pass', (req, res)=>{
+	var pass = req.query.pass;
+	console.log('pass');
+	var start = new Date();
+	
+	bcrypt.genSalt(14, function(err, salt) {
+		//hash(data, salt, progress, cb)
+	    bcrypt.hash(pass, salt, progressCallback, function(err, hash) {
+	        // Store hash in your password DB.
+	    
+		// execution time 
+	    var end = new Date() - start;
+	    console.info("Execution time: %dms", end);
+	    });
+	});
+});
+
+function progressCallback(p) {
+	console.log(Math.floor(p*100) + '%' );
+
+
+}
 
 module.exports = router;
